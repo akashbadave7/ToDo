7654e19387ad77b02f5e834cd54e6097715e0a41
akash <akashbadave7>
2017-12-14 09:39:00 +0530

    commit

---
+++ src/test/java/com/bridgeit/Controller/NoteControllerTest.java
@@ -0,0 +1,21 @@
+package com.bridgeit.Controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= {TestContext.class,WebApplicationContext.class})
+@WebAppConfiguration
+public class NoteControllerTest  {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+}
